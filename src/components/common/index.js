3e868112dc4e6e62da902a3f8bfa8765ba84e05f
Mariano Rodriguez <rdzc.mariano@gmail.com>
2026-01-14 16:36:11 -0300

    feat: barrel export

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+/**
+ * Common UI Components
+ * Centralized exports for all reusable UI primitives
+ */
+
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Tag } from './Tag';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
