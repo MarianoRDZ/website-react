5eceb107237a5db49f098cc225a521cae637d7d1
Mariano Rodriguez <rdzc.mariano@gmail.com>
2026-01-15 20:26:45 -0300

    feat: create reusable tech icon components

--- src/components/common/icons/index.js
+++ src/components/common/icons/index.js
@@ -1,3 +1,4 @@
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as LinkedInIcon } from './LinkedInIcon';
 export { default as EmailIcon } from './EmailIcon';
+export * from './TechIcons';
